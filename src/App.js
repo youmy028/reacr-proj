01486e0790d0e22cec829492453a7bc2ff56f994
youmy028 <youmy028@naver.com>
2022-12-17 20:21:13 +0900

    팝업과 닫기버튼

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import StopWatch from './StopWatch'
 import NumberCounter from './NumberCounter'
+import Popup from './Popup';
 function App() {
   return (
     <>
       {/* <StopWatch/> */}
       {/* <NumberCounter/> */}
-      <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
-      <button className="btn btn-active btn-primary">Button</button>
+      {/* <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1> */}
+      {/* <button className="btn btn-active btn-primary">Button</button> */}
+      <Popup/>
     </>
   );
 }
