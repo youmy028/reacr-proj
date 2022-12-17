e0b56dfb5f7ddaf2f3c8e7ddf29be79f8fac4ff1
youmy028 <youmy028@naver.com>
2022-12-17 19:01:53 +0900

    setTimeout 예제

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,9 +1,13 @@
-
+import React, {useState} from "react";
 function StopWatch() {
+  
+  const [num, setNum] = useState(0);
+
+  setTimeout(()=>setNum(num+1),1000);
+
   return (
     <>
-      <div>안녕</div>
-      <div>안녕</div>
+    숫자:{num}  
     </>
   );
 }
