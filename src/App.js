83d51d2cd17413c817ef58705f9f6ecc6bd1b392
youmy028 <youmy028@naver.com>
2022-12-19 16:13:41 +0900

    상품리스트, 상품명에css작업, 3번이나 작업해야해서 번거로움

--- src/App.js
+++ src/App.js
@@ -8,9 +8,26 @@ function App() {
       {/* <NumberCounter/> */}
       {/* <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1> */}
       {/* <button className="btn btn-active btn-primary">Button</button> */}
-      <Popup/>
+      {/* <Popup/> */}
+      <div style={{display:"flex",gap:"10px"}}>
+        <div style={{ display:"inline-flex", flexDirection:"column", gap:"10px" }}>
+          <img src="https://picsum.photos/id/201/400/400"/>
+          <div style={{textAlign:"center", fontWeight:"bold",color:"#454545"}}>MAC BOOK AIR</div>
+          <div style={{textAlign:"center"}}>4,340,000</div>
+        </div>
+        <div style={{display:"inline-flex", flexDirection:"column", gap:"10px"}}>
+          <img src='https://picsum.photos/id/1/400/400'></img>
+          <div style={{textAlign:"center", fontWeight:"bold",color:"#454545"}}>MAC BOOK PRO</div>
+          <div style={{textAlign:"center"}}>4,340,000</div>
+        </div>
+        <div style={{display:"inline-flex", flexDirection:"column", gap:"10px"}}>
+          <img src='https://picsum.photos/id/2/400/400'></img>
+          <div style={{textAlign:"center", fontWeight:"bold",color:"#454545"}}>MAC BOOK PRO PLUS</div>
+          <div style={{textAlign:"center"}}>4,340,000</div>
+        </div>
+      </div>  
     </>
   );
 }
 
-export default App;
+export default App; 
