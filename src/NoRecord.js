73dbb493c9412656b848fb41a6dad4fe422b6e8e
youmy028 <youmy028@naver.com>
2022-12-19 19:53:36 +0900

    기록된 숫자들이 리스트 형태로 출력

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,27 +1,26 @@
 import React, {useState} from "react";
 function NoRecord(){
-  const [arr, setArr] = useState([1,2,3,4,5]);
-
-  setTimeout(() =>{
-    setArr([...arr, arr.length+1])
-  },1000);
 
   const [no, setNo] = useState(0);
   
-  const [recordedNo, setRecordedNo] = useState(0);
+  
+  const [recordedNos, setRecordedNos] = useState([10,20,30]);
   const saveNo = () => {
-    setRecordedNo(no);
+    setRecordedNos([...recordedNos, no]);
   }
+  const li = recordedNos.map((el, index) => <li key={index}>- {el}</li>);
 
   return( 
     <>
-      {arr.join(",")}
-      {/* <h1>숫자기록</h1>
+      <h1>숫자기록</h1>
       <hr/>
       <input type="number" className="input input-bordered input-warning w-full max-w-xs" value={no} onChange={(e) => setNo(e.target.valueAsNumber)} />
       <button type="button" className="btn btn-active"  onClick={saveNo}>기록</button>
       <hr />
-  기록된 숫자 : {recordedNo}*/}
+      <h1>기록된 숫자 v1 : [{recordedNos.join(",")}]</h1>
+      <hr />
+      <h1>기록된 숫자 v2 : <ul>{li}</ul></h1>
+      <h1>기록된 숫자 v2-2 : <ul>{recordedNos.map((el,index)=> (<li key={index}>-{el}</li>))}</ul></h1>
     </>
     );
 }
