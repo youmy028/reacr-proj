e27b139431d4716e27d39af1f9363638551f17d9
youmy028 <youmy028@naver.com>
2022-12-27 21:34:32 +0900

    state를 사용해서 함수의 특정부분 재실행 방지

--- src/Notice.js
+++ src/Notice.js
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "https://cdn.skypack.dev/classnames";
+
+function NotifyOnce({ children }) {
+  const [visible, setVisible] = useState(false);
+  const [workDone, setWorkDone] = useState(false);
+  const onChange = () => setWorkDone(true);
+  if (workDone == false) {
+    setTimeout(function () {
+      setVisible(true);
+    }, 1000);
+
+    setTimeout(function () {
+      setVisible(false);
+    }, 3000);
+
+    onChange();
+  }
+
+  return (
+    <div
+      className={classnames(
+        "fixed transition-all right-[10px]",
+        { "top-[-60px]": !visible },
+        { "top-[10px]": visible }
+      )}
+    >
+      {children}
+    </div>
+  );
+}
 
 function Alert({ color: color_, children }) {
   const color = color_ ?? "white";
@@ -17,9 +47,15 @@ function Alert({ color: color_, children }) {
 function Notice() {
   return (
     <>
-      <Alert>"안녕" 반가워</Alert>
-      <br />
-      <Alert color="pink">지금은 수업중이야</Alert>
+      <NotifyOnce>
+        <Alert>"안녕" 반가워</Alert>
+      </NotifyOnce>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio
+        eveniet unde in placeat quae, perferendis corporis rerum dolorum iure
+        molestias sunt labore. Minima, alias laboriosam voluptas commodi
+        voluptate numquam?
+      </div>
     </>
   );
 }
